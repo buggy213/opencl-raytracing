9d2b1bbd9d964f8820e26928c5331717484333c9
joshua <jyou12@berkeley.edu>
2025-03-02 01:01:28 -0800

    rip out vortex specific things

--- build.rs
+++ build.rs
@@ -9,18 +9,4 @@ fn main() {
     else {
         panic!("unable to get metadata from embree4 dependency");
     }
-    println!("cargo:rerun-if-env-changed=POCL_RT_PATH");
-    println!("cargo:rerun-if-env-changed=VORTEX_DRV_STUB_PATH");
-    // ugly hack: if environment variables from env.sh are set, then link against them
-    if let Ok(e) = env::var("POCL_RT_PATH") {
-        if let Ok(f) = env::var("VORTEX_DRV_STUB_PATH") {
-            let mut pocl_runtime = PathBuf::from(e);
-            pocl_runtime.push("lib");
-            println!("cargo:rustc-link-search=native={}", pocl_runtime.display());
-
-            let vortex_driver_stub = PathBuf::from(f);
-            println!("cargo:rustc-link-search=native={}", vortex_driver_stub.display());
-            println!("cargo:rustc-link-lib=vortex");
-        }
-    }
 }
