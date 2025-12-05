fa2c9e81369edb5d80ff28e4504ab66e56eccd49
joshua <jyou12@berkeley.edu>
2025-12-05 15:18:53 -0800

    fix build on linux

--- crates/raytracing-optix/build.rs
+++ crates/raytracing-optix/build.rs
@@ -13,9 +13,28 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=dylib=raytracing_optix");
 
+    let include_path = if cfg!(target_os = "windows") {
+        format!("-I \"{}\"", dst.display())
+    } else {
+        format!("-I{}", dst.display())
+    };
+
+    let Ok(optix_path) = std::env::var("OPTIX90_PATH") else {
+        panic!("OPTIX90_PATH is not set");
+    };
+
+    let optix_include_path = PathBuf::from(optix_path).join("include");
+    let optix_include_path = if cfg!(target_os = "windows") {
+        format!("-I \"{}\"", optix_include_path.display())
+    } else {
+        format!("-I{}", optix_include_path.display())
+    };
+
+    
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg(format!("-I \"{}\"", dst.display()))
+        .clang_arg(include_path)
+        .clang_arg(optix_include_path)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("unable to generate bindings");
