0e6d0eb51fca09ce132823542561557100037cdb
joshua <jyou12@berkeley.edu>
2026-01-17 22:20:53 -0800

    extremely basic kernel

--- crates/raytracing-optix/kernels/normals.cu
+++ crates/raytracing-optix/kernels/normals.cu
@@ -1,6 +1,12 @@
 // Ray-generation w/ pinhole camera; only performs primary visibility and only calculates geometric normals
+#include <optix_device.h>
+#include "params.cuh"
 
-// populates buffer
+// populates buffer w/ debug values for now
 extern "C" __global__ void __raygen__debug() {
-    
+    uint3 dim = optixGetLaunchDimensions();
+    uint3 tid = optixGetLaunchIndex();
+    uint2 *debug = pipeline_params.debug;
+
+    debug[tid.y * dim.x + tid.x] = uint2 { tid.x, tid.y };
 }
---
+++ crates/raytracing-optix/kernels/params.cuh
@@ -0,0 +1,4 @@
+struct PipelineParams {
+    uint2* debug;
+};
+extern "C" __constant__ PipelineParams pipeline_params;
